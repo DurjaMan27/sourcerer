244b5f077e8bc925c71c92e3455a6c65954a3d3a
Varun Rao <varunvellorerao@gmail.com>
2023-12-27 16:16:36 -0500

    Updated name to urlpatterns instead of urls

--- sourcerer/sourcerer/urls.py
+++ sourcerer/sourcerer/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('sourcing/', include("sourcing.urls"))
+    path("admin/", admin.site.urls),
+    path("", include("sourcing.urls"))
 ]
