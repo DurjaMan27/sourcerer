0b92bb2cc2d65ab77230b44fdadd1a96a7cd2a82
Varun Rao <varunvellorerao@gmail.com>
2023-12-27 15:27:50 -0500

    Updated name of app

--- sourcerer/sourcerer/settings.py
+++ sourcerer/sourcerer/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'sourcerer',
+    'sourcing',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
