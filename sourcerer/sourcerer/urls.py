736be8011a383ad43514e3be73b1d763cc1c56fd
Varun Rao <varunvellorerao@gmail.com>
2023-12-27 16:09:27 -0500

    Updated check to proper url

--- sourcerer/sourcerer/urls.py
+++ sourcerer/sourcerer/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sourcing/', include("hello.urls"))
+    path('sourcing/', include("sourcing.urls"))
 ]
