e543412b45bf0352e73c09c343eee927a43958d3
Varun Rao <varunvellorerao@gmail.com>
2023-12-27 16:21:20 -0500

    testing 2FA

--- test.py
+++ test.py
@@ -4,5 +4,5 @@ topicInput = "" # string of the topic
 citationFormat = "" # string of the format of citation (MLA, APA, Chicago)
 numberSources = 0 # how many sources are to be returned (maximum of 6)
 
-
+print("testing 2FA")
 
