2cf18f428e160f812f3a3d7633b895e2a90e30e9
Varun Rao <varunvellorerao@gmail.com>
2023-12-27 21:51:12 -0500

    Added results function that displays page

--- sourcerer/sourcing/views.py
+++ sourcerer/sourcing/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django import forms
 
 # Create your views here.
@@ -10,15 +11,17 @@ class NewSearchForm(forms.Form):
         ('APA', 'APA'),
         ('Chicago', 'Chicago'),
     ]
-
     topicQuestion = forms.CharField(label="Topic", widget=forms.TextInput(attrs={'placeholder': 'Research Question'}), required=True)
     startingBid = forms.IntegerField(label="Number of Sources", required=False)
     citationFormat = forms.ChoiceField(label="Citation Format", choices=CATEGORY_CHOICES, required=True)
 
 def homepage(request):
     if request.method == 'POST':
-        print("hello")
+        return HttpResponseRedirect(reverse('results'))
     else:
         return render(request, "sourcing/homepage.html", {
             'form': NewSearchForm
         })
+
+def results(request):
+    return render(request, "sourcing/results.html")
