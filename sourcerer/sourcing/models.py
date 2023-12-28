02cbe772e32c55a68f983f6594a411ec381b0d6f
Varun Rao <varunvellorerao@gmail.com>
2023-12-28 14:01:10 -0500

    Minor change

--- sourcerer/sourcing/models.py
+++ sourcerer/sourcing/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
-
 class Result(models.Model):
     title = models.TextField()
     sourceURL = models.URLField()
