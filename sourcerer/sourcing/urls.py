8b170078b084782ac400ac6e45ac4ba3f121992e
Varun Rao <varunvellorerao@gmail.com>
2023-12-28 13:57:03 -0500

    Added login, lgout, and register paths

--- sourcerer/sourcing/urls.py
+++ sourcerer/sourcing/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path("results/<int:searchID>", views.results, name="results")
+    path("results/<int:searchID>", views.results, name="results"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", views.register, name="register"),
 ]
