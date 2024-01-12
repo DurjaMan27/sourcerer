c758295729330741fd6acf336ebab0dee62af7a6
Varun Rao <varunvellorerao@gmail.com>
2024-01-12 12:05:49 -0500

    Trying to add style attributes w/o changing field

--- sourcerer/sourcing/views.py
+++ sourcerer/sourcing/views.py
@@ -20,8 +20,10 @@ class NewSearchForm(forms.Form):
         ('Chicago', 'Chicago'),
     ]
     topicQuestion = forms.CharField(label="Topic", widget=forms.TextInput(attrs={'placeholder': 'Research Question', 'style': 'width: 600px; display: block; align: center; margin: 20px;'}), required=True)
-    numSources = forms.IntegerField(label="Number of Sources", required=False, widget=forms.TextInput(attrs={'style': 'width: 100px; display: block; align: center; margin: 20px;'}))
-    citationFormat = forms.ChoiceField(label="Citation Format", choices=CATEGORY_CHOICES, required=True, widget=forms.TextInput(attrs={'style':'display: block; align: center; margin: 20px;'}))
+    #numSources = forms.IntegerField(label="Number of Sources", required=False, widget=forms.TextInput(attrs={'style': 'width: 100px; display: block; align: center; margin: 20px;'}))
+    #citationFormat = forms.ChoiceField(label="Citation Format", choices=CATEGORY_CHOICES, required=True, widget=forms.TextInput(attrs={'style':'display: block; align: center; margin: 20px;'}))
+    numSources = forms.IntegerField(label="Number of Sources", required=True, attrs={'style': 'width: 100px; display: block;'})
+    citationFormat = forms.ChoiceField(label="Citation Format", choices=CATEGORY_CHOICES, required=True, attrs={'style':'width: 100px; display: block;'})
 
 
 def login_view(request):
