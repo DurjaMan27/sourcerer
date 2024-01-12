f7dd42aaaf7892b26e2dfc2dc0d9620017703609
Varun Rao <varunvellorerao@gmail.com>
2024-01-12 13:52:36 -0500

    Removed resultsAuth and homepageAuth

--- sourcerer/sourcing/urls.py
+++ sourcerer/sourcing/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path("<str:username>", views.homepageAuthenticated, name="homepage-authenticated"),
     path("results/<int:searchID>", views.results, name="results"),
-    path("<str:username>/results/<int:searchID>", views.resultsAuthenticated, name="results-authenticated"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
