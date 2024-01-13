aa450882671dd1ae73c9696b8581c8228c3b2e2b
Varun Rao <varunvellorerao@gmail.com>
2024-01-13 11:40:44 -0500

    Random file for testing code

--- test.py
+++ test.py
@@ -6,3 +6,22 @@ numberSources = 0 # how many sources are to be returned (maximum of 6)
 
 print("testing 2FA")
 
+response = "Title: here Citation: citation Title: here2 Citation: citation2"
+
+miniresponse = response
+for i in range(numSources):
+    j = 0
+    while j != i:
+        miniresponse = response[response.find("Title:")+1:]
+
+    miniresponse = miniresponse[miniresponse.find("Title:"):]
+
+    title = miniresponse[miniresponse.find("Title:") : miniresponse.find("Link:")]
+    url = miniresponse[miniresponse.find("Link:")+5 : miniresponse.find("Summary:")]
+    summary = miniresponse[miniresponse.find("Summary:") : miniresponse.find("Citation:")]
+    citation = miniresponse[miniresponse.find("Citation:") : miniresponse[1:].find("Title:")]
+
+    title = title.replace("*", "")
+    url = url.replace("*", "")
+    summary = summary.replace("*", "")
+    citation = citation.replace("*", "")
